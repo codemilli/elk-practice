437a04325d606a9137c6072a0d71a488db3c7aa2
hckrmoon <hckrmoon@gmail.com>
2017-04-30 00:24:01 +0900

    server elastic code

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 var express = require('express');
-var elasticsearch = require('elasticsearch');
+var ELASTICSEARCH = require('elasticsearch');
 var app = express();
+
+var CLIENT = new ELASTICSEARCH.Client({
+  host: 'localhost:9200',
+  apiVersion: '5.0'
+});
+
+CLIENT.get({
+  index: 'simpsons',
+  type: 'episode',
+  id: 1
+});
